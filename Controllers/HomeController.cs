d64942a9bd5e98d62377c806c09bf840a4932013
Ixxith <60019877+Ixxith@users.noreply.github.com>
2021-02-03 18:50:46 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using FilmCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SubmitMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitMovie(Movie movie)
+        {
+           if (ModelState.IsValid) { 
+            TempStorage.AddMovie(movie);
+            return View("Index");
+            }
+            return View();
+        }
+
+        public IActionResult ViewMovies()
+        {
+            return View(TempStorage.Movies);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmCollection.Models
+{
+    public class Movie
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [StringLength(25, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        public string Notes { get; set; }
+    }
+}
